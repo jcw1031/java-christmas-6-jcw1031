3d6145311223b558da58a8ecd730117f9814a8d5
jcw1031 <jcw001031@gmail.com>
2023-11-11 20:26:07 +0900

    feat: 메뉴 가격 추가

--- src/main/java/christmas/domain/reservation/Menu.java
+++ src/main/java/christmas/domain/reservation/Menu.java
@@ -10,28 +10,30 @@ import static christmas.domain.reservation.MenuType.MAIN;
 
 public enum Menu {
 
-    MUSHROOM_SOUP("양송이수프", APPETIZER),
-    TAPAS("타파스", APPETIZER),
-    CAESAR_SALAD("시저샐러드", APPETIZER),
+    MUSHROOM_SOUP("양송이수프", APPETIZER, 6_000),
+    TAPAS("타파스", APPETIZER, 5_500),
+    CAESAR_SALAD("시저샐러드", APPETIZER, 8_000),
 
-    T_BONE_STEAK("티본스테이크", MAIN),
-    BARBECUE_RIBS("바비큐립", MAIN),
-    SEAFOOD_PASTA("해산물파스타", MAIN),
-    CHRISTMAS_PASTA("크리스마스파스타", MAIN),
+    T_BONE_STEAK("티본스테이크", MAIN, 55_000),
+    BARBECUE_RIBS("바비큐립", MAIN, 54_000),
+    SEAFOOD_PASTA("해산물파스타", MAIN, 35_000),
+    CHRISTMAS_PASTA("크리스마스파스타", MAIN, 25_000),
 
-    CHOCOLATE_CAKE("초콜릿케이크", DESSERT),
-    ICE_CREAM("아이스크림", DESSERT),
+    CHOCOLATE_CAKE("초콜릿케이크", DESSERT, 15_000),
+    ICE_CREAM("아이스크림", DESSERT, 5_000),
 
-    ZERO_COLA("제로콜라", DRINK),
-    RED_WINE("레드와인", DRINK),
-    CHAMPAGNE("샴페인", DRINK);
+    ZERO_COLA("제로콜라", DRINK, 3_000),
+    RED_WINE("레드와인", DRINK, 60_000),
+    CHAMPAGNE("샴페인", DRINK, 25_000);
 
     private final String name;
     private final MenuType menuType;
+    private final int price;
 
-    Menu(String name, MenuType menuType) {
+    Menu(String name, MenuType menuType, int price) {
         this.name = name;
         this.menuType = menuType;
+        this.price = price;
     }
 
     public static Optional<Menu> of(String name) {
