192328cd6bf496fcf5a7b0087556ecf00a6088a9
jcw1031 <jcw001031@gmail.com>
2023-11-14 14:09:42 +0900

    feat: 증정 이벤트 할인 금액 로직

--- src/main/java/christmas/domain/benefits/discount/policy/GiveawayDiscountPolicy.java
+++ src/main/java/christmas/domain/benefits/discount/policy/GiveawayDiscountPolicy.java
@@ -1,6 +1,9 @@
 package christmas.domain.benefits.discount.policy;
 
+import christmas.domain.benefits.Giveaway;
 import christmas.domain.benefits.discount.Discount;
+import christmas.domain.benefits.discount.DiscountType;
+import christmas.domain.reservation.Menu;
 import christmas.domain.reservation.Reservation;
 
 import java.util.Optional;
@@ -9,6 +12,12 @@ public class GiveawayDiscountPolicy implements DiscountPolicy {
 
     @Override
     public Optional<Discount> calculateDiscountAmount(Reservation reservation) {
+        int totalOrderAmount = reservation.getTotalOrderAmount();
+        if (totalOrderAmount >= Giveaway.GIVEAWAY_EVENT_APPLY_THRESHOLD) {
+            Menu giveawayMenu = Menu.getGiveaway();
+            Discount discount = Discount.of(DiscountType.GIVEAWAY, giveawayMenu.getPrice());
+            return Optional.of(discount);
+        }
         return Optional.empty();
     }
 }
