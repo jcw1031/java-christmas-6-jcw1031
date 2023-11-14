08d3aba39976fa784ae4c46d5a79488a82ddc0fe
jcw1031 <jcw001031@gmail.com>
2023-11-14 16:35:36 +0900

    test: 평일/주말 할인 적용 로직 테스트

--- src/test/java/christmas/domain/benefits/discount/DiscountPolicyTest.java
+++ src/test/java/christmas/domain/benefits/discount/DiscountPolicyTest.java
@@ -2,6 +2,7 @@ package christmas.domain.benefits.discount;
 
 import christmas.converter.OrderMenusConverter;
 import christmas.domain.benefits.discount.policy.DDayDiscountPolicy;
+import christmas.domain.benefits.discount.policy.WeekDiscountPolicy;
 import christmas.domain.reservation.Reservation;
 import christmas.dto.OrderMenuDto;
 import christmas.dto.OrderMenusDto;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DiscountPolicyTest {
 
     DDayDiscountPolicy dDayDiscountPolicy = new DDayDiscountPolicy();
+    WeekDiscountPolicy weekDiscountPolicy = new WeekDiscountPolicy();
     Reservation reservation;
 
     @BeforeEach
@@ -66,6 +70,59 @@ class DiscountPolicyTest {
         reservation.generateOrders(orderMenuDtoList);
 
         Optional<Discount> discount = dDayDiscountPolicy.calculateDiscountAmount(reservation);
+
+        // then
+        assertThat(discount).isEmpty();
+    }
+
+    @DisplayName("평일/주말 할인을 계산한다.")
+    @CsvSource(textBlock = """
+            1, 티본스테이크-1
+            25, 초코케이크-1
+            """)
+    @ParameterizedTest
+    void weekDiscount(int visitDay, String orderMenusInput) {
+        // given
+        OrderMenusDto orderMenusDto = OrderMenusConverter.convert(List.of(orderMenusInput));
+        List<OrderMenuDto> orderMenuDtoList = orderMenusDto.orderMenus();
+
+        // when
+        reservation.generateVisitDate(visitDay);
+        reservation.generateOrders(orderMenuDtoList);
+
+        DiscountType discountType = DiscountType.WEEKDAY;
+        DayOfWeek dayOfWeek = LocalDate.of(2023, 12, visitDay).getDayOfWeek();
+        if (dayOfWeek.equals(DayOfWeek.FRIDAY) || dayOfWeek.equals(DayOfWeek.SATURDAY)) {
+            discountType = DiscountType.WEEKEND;
+        }
+
+
+        Optional<Discount> discount = weekDiscountPolicy.calculateDiscountAmount(reservation);
+
+        // then
+        assertThat(discount).isNotEmpty();
+        assertThat(discount).get()
+                .extracting("type").isEqualTo(discountType);
+        assertThat(discount).get()
+                .extracting("amount").isEqualTo(-2_023);
+    }
+
+    @DisplayName("할인 메뉴가 없는 경우 평일/주말 할인이 적용되지 않는다.")
+    @CsvSource(textBlock = """
+            1, 초코케이크-1
+            25, 티본스테이크-1
+            """)
+    @ParameterizedTest
+    void weekDiscountNone(int visitDay, String orderMenusInput) {
+        // given
+        OrderMenusDto orderMenusDto = OrderMenusConverter.convert(List.of(orderMenusInput));
+        List<OrderMenuDto> orderMenuDtoList = orderMenusDto.orderMenus();
+
+        // when
+        reservation.generateVisitDate(visitDay);
+        reservation.generateOrders(orderMenuDtoList);
+
+        Optional<Discount> discount = weekDiscountPolicy.calculateDiscountAmount(reservation);
 
         // then
         assertThat(discount).isEmpty();
